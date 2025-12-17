2659e122e2fdf226f62cad3bded12b04b36b8dd5
Hrithik <learningdemo00@gmail.com>
2025-12-17 23:15:42 +0530

    Before 2.0 Owner filter in UI

--- dags/my_dag_v1_0_0_1.py
+++ dags/my_dag_v1_0_0_1.py
@@ -2,9 +2,14 @@ from airflow import DAG #type:ignore
 from datetime import datetime
 from airflow.operators.bash import BashOperator #type:ignore
 
+default_args = {
+        'owner':'Marc'
+}
+
 with DAG(
-        dag_id = 'my_dag',
+        dag_id = 'my_dag_v1_0_0_1',
         start_date = datetime(2025,1,1),
+        default_args = default_args,
         schedule_interval = '@daily',
         catchup = False
          ) as dag :
