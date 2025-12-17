be7c9b2793963eb570ffbd1b4150c7d9f15b1283
Hrithik <learningdemo00@gmail.com>
2025-12-18 00:35:24 +0530

    Task startdate Note No use

--- dags/my_dag_v1_0_0_0.py
+++ dags/my_dag_v1_0_0_0.py
@@ -5,19 +5,21 @@ from airflow.operators.bash import BashOperator #type:ignore
 
 with DAG(
         dag_id = 'my_dag_v1_0_0_0',
-        start_date = datetime.now(),
+        start_date = datetime(21,1,1),
         schedule_interval = '*/1 * * * *',
         catchup = False
          ) as dag :
         
         task_a = BashOperator(
                 owner = 'Macr',
+                start_date = datetime(21,1,2),
                 task_id = "taks_a",
                 bash_command = "echo 'Task A'"
         )
 
         task_c = BashOperator(
                 owner = 'Krishna',
+                start_date = datetime(21,1,3),
                 task_id = "taks_c",
                 bash_command = "echo 'Task C'"
         )
