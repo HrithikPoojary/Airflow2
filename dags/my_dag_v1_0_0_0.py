271328156b3ccd6d9fe86b020c6baacb78ce2c72
Hrithik <learningdemo00@gmail.com>
2025-12-17 23:18:19 +0530

    Task Owner

--- dags/my_dag_v1_0_0_0.py
+++ dags/my_dag_v1_0_0_0.py
@@ -2,29 +2,28 @@ from airflow import DAG #type:ignore
 from datetime import datetime
 from airflow.operators.bash import BashOperator #type:ignore
 
-default_args = {
-        'owner' : 'airflow'
-}
 
 with DAG(
         dag_id = 'my_dag_v1_0_0_0',
         start_date = datetime(2025,1,1),
-        default_args = default_args,
         schedule_interval = '@daily',
         catchup = False
          ) as dag :
         
         task_a = BashOperator(
+                owner = 'Macr',
                 task_id = "taks_a",
                 bash_command = "echo 'Task A'"
         )
 
         task_c = BashOperator(
+                owner = 'Krishna',
                 task_id = "taks_c",
                 bash_command = "echo 'Task C'"
         )
 
         task_b = BashOperator(
+                owner = 'Luffy',
                 task_id = "task_b",
                 bash_command = "echo 'Task B'"
         )
