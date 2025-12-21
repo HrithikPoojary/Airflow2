c84bea44f63e3ca64f1e9d88e27d92e2c6832d2b
Hrithik <learningdemo00@gmail.com>
2025-12-21 13:10:29 +0530

    Fist before pool default pool 128

--- dags/my_dag_v1_0_0_0.py
+++ dags/my_dag_v1_0_0_0.py
@@ -2,6 +2,7 @@ from airflow import DAG #type:ignore
 from datetime import datetime,timedelta
 from airflow.operators.bash import BashOperator #type:ignore
 from airflow.operators.python import PythonOperator #type:ignore
+from airflow.utils.helpers import cross_downstream #type:ignore
 
 def _my_func(execution_date):
         if execution_date.day == 5 :
@@ -11,26 +12,51 @@ with DAG(
         dag_id = 'DownStream',
         start_date = datetime(25,12,10),
         schedule_interval = '@daily',
-        catchup = True
+        catchup = False
 ) as dag:
-        task_a = BashOperator(
-                task_id = 'task_a',
+        extract_a = BashOperator(
+                task_id = 'extract_a',
                 bash_command = "echo 'task A' && sleep 5",
                 wait_for_downstream = True
         )
 
-        task_b = BashOperator(
-                task_id = 'task_b',
+        extract_b = BashOperator(
+                task_id = 'extract_b',
+                bash_command = "echo 'task A' && sleep 5",
+                wait_for_downstream = True
+        )
+
+        process_a = BashOperator(
+                task_id = 'process_a',
                 retries = 3,
                 retry_exponential_backoff = True,
                 retry_delay = timedelta(seconds=5),
-                bash_command = "echo 'task B' && exit  1"       
+                bash_command = "echo 'task B' && sleep 20"       
         )
 
-        task_c = PythonOperator(
+        process_b = BashOperator(
+                task_id = 'process_b',
+                retries = 3,
+                retry_exponential_backoff = True,
+                retry_delay = timedelta(seconds=5),
+                bash_command = "echo 'task B' && sleep 20"       
+        )
+
+        process_c = BashOperator(
+                task_id = 'process_c',
+                retries = 3,
+                retry_exponential_backoff = True,
+                retry_delay = timedelta(seconds=5),
+                bash_command = "echo 'task B' && sleep 20"       
+        )
+
+        store = PythonOperator(
                 task_id = 'task_c',
                 python_callable = _my_func,
                 depends_on_past = True
         )
 
-        task_a >> task_b >> task_c
+       # Error [extract_a,extract_b] >> [process_a,process_b,process_c] >> store
+
+        cross_downstream([extract_a,extract_b] , [process_a,process_b,process_c])
+        [process_a,process_b,process_c] >> store
