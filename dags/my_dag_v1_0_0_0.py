9031a9da8367eba010d3c73f4294b68b74fa31f6
Hrithik <learningdemo00@gmail.com>
2025-12-22 23:44:15 +0530

    Callback Functions

--- dags/my_dag_v1_0_0_0.py
+++ dags/my_dag_v1_0_0_0.py
@@ -3,9 +3,25 @@ from datetime import datetime,timedelta
 from airflow.operators.bash import BashOperator  #type:ignore
 from airflow.operators.python import PythonOperator #type:ignore
 
+'''
+on_success_callback
+on_failure_callback
+on_execute_callback
+on_retry_callback
+We will get context we can use it in the callable function
+To track Airflow  >> logs
+'''
+
 def _my_func(execution_date):
         if execution_date.day == 5:
                 raise ValueError("Error")
+def _task_a_sucess(context):
+        print(context)
+        print(f"Luffy's Dag Run {context['dag']}")
+
+def _task_b_failure(context):
+        print(context)
+        print(f"Failed due to {context['exception']}")
 
 with DAG(
         dag_id = 'DownStream',
@@ -16,14 +32,16 @@ with DAG(
         task_a = BashOperator(
                 task_id = 'task_a',
                 bash_command = "echo Task A && sleep 10",
-                execution_timeout = timedelta(seconds=9)
+                execution_timeout = timedelta(seconds=20),
+                on_success_callback=_task_a_sucess
         )
 
         task_b = BashOperator(
                 task_id = 'task_b',
-                bash_command = "echo Task B && exit 0",
+                bash_command = "echo Task B && exit 1",
                 retries = 3 ,
-                retry_delay = timedelta(seconds = 10)
+                retry_delay = timedelta(seconds = 10),
+                on_failure_callback = _task_b_failure
         )
 
         task_c = PythonOperator(
