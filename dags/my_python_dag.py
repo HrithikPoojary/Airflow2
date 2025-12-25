913b9293d16e446e520b86e46f476a58b0e51520
Hrithik <learningdemo00@gmail.com>
2025-12-25 15:12:32 +0530

    Name change of the file (Mistake)

--- dags/my_python_dag.py
+++ dags/my_python_dag.py
@@ -3,6 +3,7 @@ from airflow.decorators import task #type:ignore
 from datetime import datetime
 from typing import Dict
 from airflow.operators.python import get_current_context #type:ignore
+from airflow.operators.bash import BashOperator #type:ignore
 
 #default task_id = pythonfunction name
 @task( 
@@ -12,6 +13,7 @@ def process(my_var):
         context = get_current_context()
         print(context)
         print(f"{my_var['path']} / {my_var['filename']}")
+        return 123
 
 with DAG(
         dag_id = 'my_python_dag',
@@ -19,5 +21,10 @@ with DAG(
         schedule_interval = '@daily',
         catchup = False
 ) as dag:
+        
+        store = BashOperator(
+                task_id = 'store',
+                bash_command = "echo 'Store'"
+        )
 
-        process(Variable.get("my_setting",deserialize_json=True))   
+        process(Variable.get("my_setting",deserialize_json=True)) >> store  
