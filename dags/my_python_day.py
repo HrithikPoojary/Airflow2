78175be83f4228437b77cf112befd62d104e936d
Hrithik <learningdemo00@gmail.com>
2025-12-25 13:21:18 +0530

    context of the function using get_current_context

--- dags/my_python_day.py
+++ dags/my_python_day.py
@@ -2,14 +2,15 @@ from airflow.models import DAG ,Variable #type:ignore
 from airflow.decorators import task #type:ignore
 from datetime import datetime
 from typing import Dict
-from airflow.operators.python import PythonOperator #type:ignore
+from airflow.operators.python import get_current_context #type:ignore
 
 #default task_id = pythonfunction name
 @task( 
         task_id = 'task_a'
-     )
+)
 def process(my_var):
-        print(my_var)
+        context = get_current_context()
+        print(context)
         print(f"{my_var['path']} / {my_var['filename']}")
 
 with DAG(
