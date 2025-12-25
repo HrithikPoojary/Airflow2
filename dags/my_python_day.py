58eb408ae5a7a6d9271be1fd17f82c5f09fd1fbb
Hrithik <learningdemo00@gmail.com>
2025-12-25 12:06:37 +0530

    Arguments to the python function

--- dags/my_python_day.py
+++ dags/my_python_day.py
@@ -1,14 +1,9 @@
 from airflow.models import DAG  #type:ignore
 from datetime import datetime
 from airflow.operators.python import PythonOperator #type:ignore
-'''
-docker ps
-docker exec -it <container_id scheduler> /bin/bash
-airflow tasks test <dag_id> <task_id> <execution_dt> 
-'''
 
-def _my_fun():
-     print("Hello Task A")
+def _process(path,filename):
+     print(f"{path}/{filename}")
 
 
 with DAG(
@@ -20,5 +15,6 @@ with DAG(
 
      task_a = PythonOperator(
           task_id = 'task_a',
-          python_callable = _my_fun
+          python_callable = _process,
+          op_args = ['usr/local/aiflow' , 'test.csv']
      )   
