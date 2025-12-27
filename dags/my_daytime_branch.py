5306094896f6bc0d444c4741bd3a9333fff312cc
Hrithik <learningdemo00@gmail.com>
2025-12-27 23:20:08 +0530

    External task sensor demo

--- dags/my_daytime_branch.py
+++ dags/my_daytime_branch.py
@@ -1,21 +1,33 @@
-# if you run manually and after that if run using TiggerDagRunOperator automaticaly you get some error.
-
 from airflow.models import DAG  #type:ignore
 from datetime import datetime
 from airflow.operators.bash import BashOperator #type:ignore
+from airflow.sensors.external_task import ExternalTaskSensor #type:ignore
 
 default_args = {
         'start_date' : datetime(25,12,20),
-        'schedule_interval' : None
+        'schedule_interval' : '@daily'
 }
 with DAG(
         dag_id = 'my_datetime_dag',
         default_args = default_args,
         catchup = False
 ) as dag:
-        
-        task = BashOperator(
-                task_id = 'task',
-                bash_command = "echo {{dag_run.conf['path']}}"
+        # Important - you have to make sure that the execution dates between the dag where- 
+        # -the external task sensor is and the dag where the task you are waiting for are equal.
+        # otherwise external task sensor will run forever.
+        # parent dag execution date == external task sensor execution date 
+
+        #Airflow >> my_datetime_dag >> log -> Poking for parent_dag.end on (Execution_dt)2025-12-26T17:32:07.479372+00:00 ... 
+        waiting_for_task = ExternalTaskSensor(
+                task_id = 'waiting_for_task',
+                external_dag_id = 'parent_dag',
+                external_task_id = 'end'
         )
+        
+        training_ml = BashOperator(
+                task_id = 'task',
+                bash_command = " training_ml "
+        )
+
+        waiting_for_task >> training_ml
         
