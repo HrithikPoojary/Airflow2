51c189c499be12d93f255be033a1362b429e15ff
Hrithik <learningdemo00@gmail.com>
2025-12-27 15:53:05 +0530

    Subdag: conf propogate skipped state

--- dags/subdag.py
+++ dags/subdag.py
@@ -3,13 +3,12 @@ from airflow.operators.bash import BashOperator #type:ignore
 
 
 def subdag_factory(parent_dag_id,subdag_id,default_args):
-        default_args['pool'] = 'training_ml'
         with DAG(
                 dag_id = f"{parent_dag_id}.{subdag_id}",
                 default_args = default_args
         ) as dag:
                 
-                training_a = BashOperator(task_id = 'training_a',bash_command = "echo 'training_a' ")
+                training_a = BashOperator(task_id = 'training_a',bash_command = "echo {{dag_run.conf['output']}} ")
                 training_b = BashOperator(task_id = 'training_b',bash_command = "echo 'training_b' ")
                 training_c = BashOperator(task_id = 'training_c',bash_command = "echo 'training_c' ")
 
